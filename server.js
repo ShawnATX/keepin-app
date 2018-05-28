90baa9b1a4528404039a82df47e83660adcf6641
Shawn W <37752327+ShawnATX@users.noreply.github.com>
2018-05-27 22:51:15 -0500

    building

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var app = require('./app');
-var port = process.env.PORT || 3000;
+var express = require('express'); // ExperssJS Framework
+var app = express(); // Invoke express to variable for use in application
+var port = process.env.PORT || 8080; // Set default port or assign a port in enviornment
+var morgan = require('morgan'); // Import Morgan Package
+var mongoose = require('mongoose'); // HTTP request logger middleware for Node.js
+var bodyParser = require('body-parser'); // Node.js body parsing middleware. Parses incoming request bodies in a middleware before your handlers, available under req.body.
+var router = express.Router(); // Invoke the Express Router
+var appRoutes = require('./app/routes/api')(router); // Import the application end points/API
+var path = require('path'); // Import path module
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
+app.use(morgan('dev')); // Morgan Middleware
+app.use(bodyParser.json()); // Body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(express.static(__dirname + '/public')); // Allow front end to access public folder
+app.use('/api', appRoutes); // Assign name to end points (e.g., '/api/management/', '/api/users' ,etc. )
